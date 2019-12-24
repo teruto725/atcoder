49f0f0ec7255203db0eeeb9f610eb2689fab309d
teruto725 <akiteruto@gmail.com>
2019-12-24 17:33:25 +0900

    add 143d

--- 143d.py
+++ 143d.py
@@ -1,29 +1,10 @@
-import copy
+
 import bisect
 N = int(input())
 L = list(map(int,input().split()))
 count = 0
 L.sort()
-for i in range(N-1):
-    for j in range(i+1,N):
-        print(L[i])
-        print(L[j])
-        maxi = L[i] + L[j] -1
-        mini = abs(L[i] - L[j]) +1
-        cL = copy.copy(L)
-        if j < i: 
-            cL.pop(i)
-            cL.pop(j)
-        else:
-            cL.pop(j)
-            cL.pop(i)
-        mi = bisect.bisect_left(cL,mini)
-        ma = bisect.bisect_left(cL, maxi)
-        ma = N - ma -1
-        print("mi"+str(mi)+"ma"+str(ma))
-        if ma >= mi:
-            count += ma-mi
-
-                 
-            
+for i in range(N-2):
+    for j in range(i+1,N-1):
+        count += bisect.bisect(L,L[i]+L[j]-1)-j-1
 print(count)
