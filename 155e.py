da8a18b9f8a9306f49ac8c4fc1a075d6c7aa6ad1
teruto725 <akiteruto@gmail.com>
2020-02-18 14:15:58 +0900

    add 155e

--- 155e.py
+++ 155e.py
@@ -7,7 +7,7 @@ dp2 = [10 for i in range(len(N)+1)]
 for i in range(1,len(N)+1):
     dp[i] = min(dp[i-1] + Y[i],dp2[i-1] + Y[i])
     dp2[i] = min(dp[i-1] + 1 + 10 - Y[i],dp2[i-1] - 1 + 10 - Y[i])
-    print(str(dp[i])+":" +str(dp2[i]))
+    #print(str(dp[i])+":" +str(dp2[i]))
 ans = min(dp[-1],dp2[-1])
 #print(dp,dp2)
 print(ans)
