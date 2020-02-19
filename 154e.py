fc687378b6fc3681fc08049696af2a46f58c9663
teruto725 <akiteruto@gmail.com>
2020-02-19 13:24:19 +0900

    commit to 154e

--- 154e.py
+++ 154e.py
@@ -1,29 +1,27 @@
-
-import math
-
-def c(n, r):
-    return math.factorial(n) // math.factorial(n - r)
-
-def Knum(k):
-    Knum = 1
-    for i in range(k):
-        Knum = Knum * (9-i)
-    return Knum
-
-N=int(input())
-K=int(input())
-l=len(str(N))
-
-
-ans = 0
-ans += c(l-1,K)*Knum(K)
-if K >1:
-    ans += c(l-1,K-1)*Knum(K-1)*int(str(N)[0]-1)
-else:
-    ans += int(str(N)[0])
-if K > 2:
-    ans += c(l-2,K-2)*Knum(K-2)*int(str(N)[0]-2)
-else:
-    ans += int(str(N)[1])
-notzero=str(N).index("0")
-
+import numpy as np
+N = input()
+K = int(input())
+L = len(N)
+dp0 = [[0]*(K+1) for i in range(L+1)]
+dp1 = [[0]*(K+1) for i in range(L+1)]
+dp0[0][0] = 1
+for i in range(1,L+1):#i桁目
+  c = int(N[i-1])
+  for j in range(K+1):#k個0を使っているとする
+    if j==0:
+      if c>0:
+        dp1[i][j] = dp1[i-1][j]+dp0[i-1][j]
+        dp0[i][j] = 0
+      else:
+        dp1[i][j] = dp1[i-1][j]
+        dp0[i][j] = dp0[i-1][j]
+    else:
+      if c>0:
+        dp1[i][j] = dp0[i-1][j-1]*(c-1)+dp1[i-1][j-1]*9+dp1[i-1][j]+dp0[i-1][j]
+        dp0[i][j] = dp0[i-1][j-1]
+      else:
+        dp1[i][j] = dp1[i-1][j-1]*9+dp1[i-1][j]
+        dp0[i][j] = dp0[i-1][j]
+print(np.array(dp0))
+print(np.array(dp1))
+print(dp0[-1][-1]+dp1[-1][-1])
