70f78e0cfdcc5e4b3514d79cb4ce0506ff59eeb5
teruto725 <akiteruto@gmail.com>
2020-03-17 15:45:56 +0900

    153e

--- 153e.py
+++ 153e.py
@@ -1,16 +1,15 @@
 import numpy as np
 H,N = map(int,input().split())
 m = list()
-dp = np.zeros(H+1)
+dp = [10**9]**H
 for i in range(N):
     m.append(list(map(int,input().split())))
 for i in range(N):
-    Hi = H-i 
     dpi = int()
     zeroflg = 0
     for j in range(len(m)):
         if (Hi + m[j][0]) >=H:
-            zeroflg +=1
+            zeroflg += 1
         else:
             
 
