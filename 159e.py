9e2404a830a37587be1231c26a6b2a8bc944cdcf
teruto725 <akiteruto@gmail.com>
2020-03-24 00:11:42 +0900

    159e

--- 159e.py
+++ 159e.py
@@ -1,6 +1,33 @@
 H,W,K = map(int,input().split())
-choco = [input().split() for l in range(H)]
-num =(2**(H))
+choco = [input() for l in range(H)]
+num =(2**(H-1))
+
+white_count = list()
+min_div_count =10**6
 for i in range(num):
-    numlist = format(num, 'b')
-    li = 
+    bin_str = format(i, 'b')
+    zero = str()
+    if H != len(bin_str):
+        for i in range(H-len(bin_str)):
+            zero += "0"
+        bin_str = zero + bin_str
+    white_count = [0 for s in bin_str if s == "1"]
+    white_count.append(0)
+    div_count=len(white_count)-1
+    j = 0
+    while j < W:
+        block_count = 0
+        for k in range(len(bin_str)):
+            if bin_str[k] == "1":
+                block_count += 1
+            white_count[block_count] += int(choco[k][j])
+            if white_count[block_count] > K:
+                div_count +=1
+                white_count = [0 for s in bin_str if s == "1"]
+                white_count.append(0)
+                break
+        else:
+            j+=1
+    if min_div_count > div_count:
+        min_div_count = div_count
+print(min_div_count)
